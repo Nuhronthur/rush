4cc1683a467c0b5b573b051bcd3a1fa126bbe5fb
Jonathan Vella <jonvella@c2r2p5.42nice.fr>
2022-02-05 16:48:55 +0100

    2nd fonction in tool

--- ex00/tool.c
+++ ex00/tool.c
@@ -6,10 +6,11 @@
 /*   By: jonvella <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 13:20:05 by jonvella          #+#    #+#             */
-/*   Updated: 2022/02/05 14:03:02 by jonvella         ###   ########.fr       */
+/*   Updated: 2022/02/05 16:47:17 by jonvella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 char	*setuppar(char *dest, char *src)
 {
 	int i;
@@ -34,4 +35,22 @@ char	*setuppar(char *dest, char *src)
 		j++;
 	}
 	return (dest);
+}
+
+char **putmalloc(char **str)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	str = malloc(sizeof(char *) * (5));
+	str[4] = "\0";
+	while (i < 4)
+	{
+		*(str + i) = malloc(sizeof(char) * (5));
+		str[i][4] = '\0';
+		i++;
+	}
+	return (str);
 }
